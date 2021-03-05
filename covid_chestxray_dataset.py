775d8b600ca8bcf3d46ddb73191b2b6fed5a8fc1
huyen <huyentls1114@gmail.com>
2021-03-05 16:43:34 +0700

    chestxray

---
+++ covid_chestxray_dataset.py
@@ -0,0 +1,18 @@
+import os
+import glob
+import shutil
+
+class CovidChestXray:
+    def __init__(self, input_folder, output_folder):
+        self.input_folder = input_folder
+        self.output_folder = output_folder
+
+        self.init_name()
+    def init_name(self):
+        self.image_folder = os.path.join(self.input_folder, "images")
+        self.mask_folder = os.path.join(self.input_folder, "annotations/lungVAE-masks")
+        self.list_mask_names = os.listdir(self.mask_folder)
+        self.list_img_names = list(x.repace(".png", ".jpg") for x in list_mask_names)
+        
+
+    
